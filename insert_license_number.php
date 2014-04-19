b29c1d3d7ee52950de48ea210d0bb2b5f4095b1f
tsallee <tsallee@mines.edu>
2014-04-19 14:43:26 -0600

    Added php file to kick off the alpr software and insert the resulting license plate number into the database.

---
+++ insert_license_number.php
@@ -0,0 +1,61 @@
+<?php
+
+	function process_license_data($image_filepath, $image_url, $image_id){
+		$db = mysqli_connect("localhost", "smartlots", "M!nesRules", "parking");
+		
+		if (mysqli_connect_errno()) {
+			echo
+				"Error: Could not connect to the database. Please try again later.";
+			exit;
+		}
+
+		$license_number = get_license_number($image_filepath, $image_url);
+		$state = NULL;
+
+		insert_license_number($license_number, $state, $image_id, $db);
+		
+	}
+
+	// Gets the license plate number from the alpr software.
+	function get_license_number($image_filepath, $image_url) {
+		$license_number = "614-VIP";		
+
+		/*
+		 *
+		 *
+		 * BRANDON: THIS IS YOUR FUNCTION TO FILL OUT
+		 *
+		 *
+		 */
+
+		return $license_number;
+	}
+
+	// Insert the license plate numbers to the database
+	function insert_license_number($license_number, $state, $image_id, $db) {
+		$insert_query = "";
+		if ( is_null($state) ) {
+			$insert_query = "insert into licenses (number, image_id) values(?, ?)";
+		} else {
+			$insert_query = "insert into licenses (number, state, image_id) values (?, ?, ?)";
+		}
+
+		$stmt = $db->prepare($insert_query);
+
+		if ( is_null($state) ) {
+			$stmt->bind_param("si", $license_number, $image_id);
+		} else {
+			$stmt->bind_param("ssi", $license_number, $state, $image_id);
+		}
+
+		if ( !($stmt->execute()) ) {
+			// Result was false (error inserting into database)
+			echo "Error: the license number was not saved correctly.";
+			$db->close();
+			exit;
+		}
+
+		$db->close();
+	}
+
+?>
--- insert_pi_data.php
+++ insert_pi_data.php
@@ -1,13 +1,22 @@
 <?php
+	require 'insert_license_number.php';
 
 	function process_pi_data($post_array, $files_array){
 		$db = mysqli_connect("localhost", "smartlots", "M!nesRules", "parking");
-		insert_pi_data($post_array, $files_array); // add parameters to this function as needed
-		// Kick off Brandon's script
+		
+		if (mysqli_connect_errno()) {
+			echo
+				"Error: Could not connect to the database. Please try again later.";
+			exit;
+		}
+
+		insert_pi_data($post_array, $files_array, $db); // add parameters to this function as needed
+		
 	}
 
-	// add parameters to this function as needed
-	function insert_pi_data($post_array, $files_array) {
+	// Inserts the relevant pi data to the database, and kicks off
+	// the 'insert_license_data' function, which will run the alpr software.
+	function insert_pi_data($post_array, $files_array, $db) {
 		$d = new DateTime();
 		list($usec, $sec) = explode(" ", microtime());
 		$timestamp = $d->format('Y-m-d_H:i:s') . '.' . substr($usec, 2);
@@ -21,6 +30,34 @@
 		echo "Pi Data Successfully Received.";
 		echo "Url: $url\n";
 		echo "Id: $id\n";
+
+		$insert_query = "insert into images (sensor_id, url) values (?, ?)";
+		$stmt = $db->prepare($insert_query);
+		$stmt->bind_param("ss", $id, $url);
+
+		if ( !($stmt->execute()) ) {
+			// Result was false (error inserting into database)
+			echo "Error: the image was not saved correctly.";
+			$db->close();
+			exit;
+		}
+
+		$select_query = "select id from images where url = ?";
+		$stmt = $db->prepare($select_query);
+		$stmt->bind_param("s", $url);
+
+		if ( !($stmt->execute()) ) {
+			echo "Error: Could not access database.";
+		}
+
+		$stmt->bind_result($image_id);
+		$stmt->fetch();
+		$stmt->free_result();
+
+		$db->close();
+
+		// Kick off the script that will convert the image to a string.
+		process_license_data($file_path, $url, $image_id);
 
 	}
 
