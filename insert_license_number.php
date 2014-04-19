371fed453fd3ee2cb1386457c79d80a9fe84f277
tsallee <tsallee@mines.edu>
2014-04-19 15:25:27 -0600

    added array for license number and state in insert_license_number.php

--- insert_license_number.php
+++ insert_license_number.php
@@ -13,15 +13,22 @@
 		$license_info = get_license_number($image_filepath, $image_url);
 		
 		$license_number = $license_info[0];
-		$state = $license_info[1];
+		$intrada_license_number = $license_info[1];
+		echo $license_number;
+		echo $intrada_license_number;
+		$state = $license_info[2];
 
 		insert_license_number($license_number, $state, $image_id, $db);
+		insert_intrada_license_number($intrada_license_number, $state, $image_id, $db);
 		
+		$db->close();
+
 	}
 
 	// Gets the license plate number from the alpr software.
 	function get_license_number($image_filepath, $image_url) {
 		$license_number = "614-VIP";
+		$intrada_license_number = "XXX-OOO";
 		$state = NULL;		
 
 		/*
@@ -34,13 +41,14 @@
 
 		$license_info = array(
 			0 => $license_number,
-			1 => $state
+			1 => $intrada_license_number,
+			2 => $state
 		);
 
 		return $license_info;
 	}
 
-	// Insert the license plate numbers to the database
+	// Insert the license plate number to the database
 	function insert_license_number($license_number, $state, $image_id, $db) {
 		$insert_query = "";
 		if ( is_null($state) ) {
@@ -63,8 +71,31 @@
 			$db->close();
 			exit;
 		}
+	}
 
-		$db->close();
+	// Insert the intrada license plate number to the database
+	function insert_intrada_license_number($license_number, $state, $image_id, $db) {
+		$insert_query = "";
+		if ( is_null($state) ) {
+			$insert_query = "insert into intrada_licenses (number, image_id) values(?, ?)";
+		} else {
+			$insert_query = "insert into intrada_licenses (number, state, image_id) values (?, ?, ?)";
+		}
+
+		$stmt = $db->prepare($insert_query);
+
+		if ( is_null($state) ) {
+			$stmt->bind_param("si", $license_number, $image_id);
+		} else {
+			$stmt->bind_param("ssi", $license_number, $state, $image_id);
+		}
+
+		if ( !($stmt->execute()) ) {
+			// Result was false (error inserting into database)
+			echo "Error: the license number was not saved correctly.";
+			$db->close();
+			exit;
+		}
 	}
 
 ?>
