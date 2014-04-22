b12efc6dc9805dc37a839eaa475f4141065724bf
Drew Koelling <akoellin@mymail.mines.edu>
2014-04-22 16:01:54 -0600

    added error checking

--- insert_fio_data.php
+++ insert_fio_data.php
@@ -2,6 +2,10 @@
 
 	function process_fio_data($post_array){
 		$db = mysqli_connect("localhost", "smartlots", "M!nesRules", "parking");
+		if (mysqli_connect_errno()) {
+			echo "Error: Could not connect to the database. Please try again later.";
+			exit;
+		}
 		insert_fio_data($post_array["id"], $post_array["carcount"], $post_array["voltage"], $post_array["temperature"], $post_array["window"], $db);
 		//echo "Fio Data Successfully Received.";
 	}
@@ -22,7 +26,11 @@
 		$statement = $db->prepare($query);
 		$battery = (($voltage-2.7/(4.23-2.7))*100);
 		$statement->bind_param("iiiiiii", $sensorID, $carcount, $battery, $previousCount, $carcount, $battery, $previousCount);
-		$statement->execute();
+		if( !($statement->execute())){
+			echo "Error. could not insert into the sensor table";
+			$db->close();
+			exit;
+		}
 		$statement->close();
 
 
@@ -30,7 +38,11 @@
 		$query = "select lotid from parking.sensormapping where sensorid=?";
 		$statement = $db->prepare($query);
 		$statement->bind_param("i", $sensorID);
-		$statement->execute();
+		if( !($statement->execute())){
+			echo "Error. could not insert into the sensormapping table";
+			$db->close();
+			exit;
+		}
 		$lotID = -1;
 		$statement->bind_result($lotID);
 		$statement->fetch(); 
@@ -39,6 +51,11 @@
 		// Get all the sensors associated to that lot. can also stop using prepared statments as of now since any query is being genrated from info already in the database
 		$query = "select sensorid from parking.sensormapping where lotid = ".$lotID;
 		$result = $db->query($query);
+		if($result == false){
+			echo "Could not pull sensors attached to lotID ".$lotID;
+			$db->close();
+			exit;
+		}
 		$lotSensors = [];
 		while($row = mysqli_fetch_array($result)){
 			array_push($lotSensors, $row["sensorid"]);
@@ -49,6 +66,11 @@
 		while(count($lotSensors)){
 			$query = "select carcount, lastcount from parking.sensor where sensorid = ".array_pop($lotSensors);
 			$result = $db->query($query);
+			if($result == false){
+				echo "Could not pull sensor data attached to sensorID";
+				$db->close();
+				exit;
+			}
 			$row = mysqli_fetch_array($result);
 			$totalChange += $row["carcount"] - $row["lastcount"];
 		}	
@@ -56,6 +78,11 @@
 		// Calculate the new value for lot
 		$query = "select carcount from lots where lotid = ".$lodID;
 		$result = $db->query($query);
+		if($result == false){
+			echo "Could not pull carcount attached to lotID ".$lotID;
+			$db->close();
+			exit;
+		}
 		$row = mysqli_fetch_array($result);
 		$newCount = $row["carcount"] + $totalChange;
 
@@ -74,7 +101,11 @@
 		$query = "insert into parking.log(sensorid, localcount, date, voltage, temp, windowdata) values (?, ?, NOW(), ?, ?, ?)";
 		$statement = $db->prepare($query);
 		$statement->bind_param("iiiis", $sensorID, $carcount, $voltage, $temperature, $window);
-		$statement->execute();
+		if( !($statement->execute())){
+			echo "Error. could not insert into the log table";
+			$db->close();
+			exit;
+		}
 		$statement->close();
 		
 	}
